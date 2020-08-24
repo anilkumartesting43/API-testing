b1beb3a9e757efb8f4aea8feb43f6ba4aaaba291
Anil <anilkumartesting43@gmail.com>
2020-08-24 19:12:17 +0530

    this is Maven

---
+++ src/test/java/EST_ZonalS/TestNgClassExp1.java
@@ -0,0 +1,25 @@
+package EST_ZonalS;
+
+import org.testng.annotations.Test;
+
+public class TestNgClassExp1 {
+	@Test
+	  public void f() {
+		  System.out.println("i'm f");
+	  }
+	  @Test
+	  public void k()
+	  {
+		  System.out.println("i'm k");
+	  }
+	  @Test
+	  public void A()
+	  {
+		  System.out.println("i'm A");
+	  }
+	  @Test
+	  public void C()
+	  {
+		  System.out.println("i'm C");
+	  }
+	}
